399fde268ed65b2ae69b6569e95e52e4849add2c
Joelle  Johnson <joellej221@gmail.com>
2023-11-10 12:15:32 -0500

    fixed errors

--- functions/src/middleware.js
+++ functions/src/middleware.js
@@ -7,15 +7,14 @@ export async function isAuthenticated(req, res, next) {
     if (!authorization) {
         res.status(401).send({ message: "No authorization token found" });
         //is that token valid
-        jwt.verify(authorization, secretKey)
-            .then(decoded => { //valid token:
-                req.locals = decoded; //attach our decoded token to the request.
-                // if so, go on
-                next();
-            })
-            .catch(err => { //invalid token:
-                res.status(401).send(err);
-            });
     }
-
-}
+        jwt.verify(authorization, secretKey, (err, decoded) => {
+            if (err) { //if invalid token:
+                res.status(401).send(err);
+                return;
+            }//else, if valid token:
+            req.locals = decoded; //attach our toekn to request
+            //if token is valid, go on to the next function in API.
+            next();
+        })
+    }
