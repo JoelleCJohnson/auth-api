bdb78e338d3f34c364782d86183754e3bc5f9bad
Joelle  Johnson <joellej221@gmail.com>
2023-11-10 12:17:44 -0500

    fixed another error

--- functions/src/middleware.js
+++ functions/src/middleware.js
@@ -6,15 +6,16 @@ export async function isAuthenticated(req, res, next) {
     //do they have a token?
     if (!authorization) {
         res.status(401).send({ message: "No authorization token found" });
-        //is that token valid
+        return;
     }
-        jwt.verify(authorization, secretKey, (err, decoded) => {
-            if (err) { //if invalid token:
-                res.status(401).send(err);
-                return;
-            }//else, if valid token:
-            req.locals = decoded; //attach our toekn to request
-            //if token is valid, go on to the next function in API.
-            next();
-        })
-    }
+    //is that token valid
+    jwt.verify(authorization, secretKey, (err, decoded) => {
+        if (err) { //if invalid token:
+            res.status(401).send(err);
+            return;
+        }//else, if valid token:
+        req.locals = decoded; //attach our toekn to request
+        //if token is valid, go on to the next function in API.
+        next();
+    });
+}
